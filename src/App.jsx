03e4940ff27b93ad5078602e6da3978aa45bbca1
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-10-23 06:25:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Countries from './components/Countries'
+
+function App() {
+
+  // State Data.
+  
+
+  return (
+    <>
+      <h1>World countries</h1>
+
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
